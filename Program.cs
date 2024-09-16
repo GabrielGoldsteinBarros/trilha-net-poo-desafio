c4d6f75f921c3a37f844c7d7206c7ec4bb43e9a3
Gabriel Goldstein <gabriel_gbarros@hotmail.com>
2024-09-16 18:17:27 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using DesafioPOO.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("123456", "Nokia Model", "111111111111111", 64);
+        Smartphone iphone = new Iphone("7891011", "iPhone Model", "222222222222222", 128);
+
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
